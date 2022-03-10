b18711246d19659eb9e772cb5727bb95393d0e98
Alejandro <75748566+Alejandroo262@users.noreply.github.com>
2022-03-10 16:03:52 +0100

    Update index.js

--- code/test/index.js
+++ code/test/index.js
@@ -2,7 +2,7 @@ const sum = require('../');
 const assert = require('assert');
 
 describe('TEST DE SUMA', () => {
-  it('Numero Positivo () => { 
+  it('Numero Positivo' () => { 
     assert.equal(sum(3, 5), 8);
   });
   it('Numero Negativo', () => {
